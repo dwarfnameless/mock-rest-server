4b280f560a868aed2e31b30b1e872728b42f72c8
Anton Bogomolov <ashteriel@ya.ru>
2025-04-22 17:23:21 +0200

    feat(tests): добавлена фикстура для очистки БД после каждого теста

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,3 +23,17 @@ async def test_app() -> AsyncGenerator[FastAPI, None]:
 def test_client(test_app: FastAPI) -> TestClient:
     """Фикстура для создания тестового клиента."""
     return TestClient(test_app)
+
+
+@pytest.fixture(autouse=True)
+async def cleanup_database() -> AsyncGenerator[None, None]:
+    """Фикстура для очистки БД после каждого теста."""
+    yield
+    from src.db import DBManager
+    from src.db.models.mock_data import Base
+
+    db_manager = DBManager()
+    if db_manager._engine is None:
+        raise ValueError("DBManager's _engine is not initialized.")
+    async with db_manager._engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
